1cda0e1951599fa204cea3477f4e8a38492d34b7
Kaikala <2507289@cognizant.com>
2026-06-17 14:03:17 +0530

    solving user repository error

--- src/main/java/com/cog/fundmatrix/repository/UserRepository.java
+++ src/main/java/com/cog/fundmatrix/repository/UserRepository.java
@@ -5,8 +5,9 @@ import com.cog.fundmatrix.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
+import java.util.UUID;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, UUID> {
 
     Optional<User> findByEmailIgnoreCase(String email);
 
