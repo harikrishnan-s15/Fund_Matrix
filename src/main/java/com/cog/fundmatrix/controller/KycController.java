1be0d41c10e617ac4dbbf82bfe96bc4046fb104a
Karthik <2507329@cognizant.com>
2026-06-17 14:15:10 +0530

    creating swagger

--- src/main/java/com/cog/fundmatrix/controller/KycController.java
+++ src/main/java/com/cog/fundmatrix/controller/KycController.java
@@ -3,8 +3,11 @@ package com.cog.fundmatrix.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+
 @Controller
 @RequestMapping("/api/kyc-record")
+@SecurityRequirement(name = "bearerAuth")
 public class KycController {
 
 	
